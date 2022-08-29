0388a80fb164ecfe0bdddfce180505cd1c7c24ce
Dat Dinh <invictaumi@gmail.com>
2022-08-29 11:36:38 +0000

    no partner added

--- hello.c
+++ hello.c
@@ -7,7 +7,9 @@
 int main(int argc, char **argv){
 
     printf("Hello, I'm Dat Dinh!\n");
+    printf("There is no partner.\n");
     printf("Major is Computer Science!\n");
+
     printf("Minor is nothing yet\n");
 
     return 0;
