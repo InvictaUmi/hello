f7fabdff809d01366be25f397bc51ebf9ffac0a6
Dat Dinh <invictaumi@gmail.com>
2022-08-29 11:21:12 +0000

    fixed standard library bug

--- hello.c
+++ hello.c
@@ -1,11 +1,13 @@
 /**
  * Basic Hello World Program
  */
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char **argv){
     printf("Hello, I'm Dat Dinh!\n");
     printf("Major is Computer Science!\n");
 
     return 0;
-    
+
 }
